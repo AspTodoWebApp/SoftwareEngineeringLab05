b00ad9640baca1613a550b17f88129348ca59066
NumanArshad <37812712+NumanArshad@users.noreply.github.com>
2018-10-11 03:44:35 +0500

    Add Person with functionality of adding multiple contacts

--- SELab5/SELab5/Controllers/PersonController.cs
+++ SELab5/SELab5/Controllers/PersonController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SELab5.Models;
+using Microsoft.AspNet.Identity;
 
 namespace SELab5.Controllers
 {
     public class PersonController : Controller
     {
+        
+        PhoneBookDbEntities _db;
+
+        public PersonController()
+        {
+            _db = new PhoneBookDbEntities();
+           
+
+        }
         // GET: Person
         public ActionResult Index()
         {
-            return View();
+            string login = User.Identity.GetUserId();
+
+            List<Person> personList = new List<Person>();
+            personList=_db.People.Where(person => person.AddedBy == login).ToList();
+            return View(personList);
         }
 
         // GET: Person/Details/5
@@ -20,6 +35,7 @@ namespace SELab5.Controllers
             return View();
         }
 
+        [Authorize]
         // GET: Person/Create
         public ActionResult Create()
         {
@@ -28,12 +44,30 @@ namespace SELab5.Controllers
 
         // POST: Person/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(Person getPerson)
         {
             try
             {
-                // TODO: Add insert logic here
+                string login = User.Identity.GetUserId();
 
+                // TODO: Add insert logic here
+                Person StorePerson = new Person();
+                StorePerson.FirstName = getPerson.FirstName;
+                StorePerson.MiddleName = getPerson.MiddleName;
+                StorePerson.LastName = getPerson.LastName;
+                StorePerson.DateOfBirth = getPerson.DateOfBirth;
+                StorePerson.FaceBookAccountId = getPerson.FaceBookAccountId;
+                StorePerson.TwitterId = getPerson.TwitterId;
+                StorePerson.LinkedInId = getPerson.LinkedInId;
+                StorePerson.HomeAddress = getPerson.HomeAddress;
+                StorePerson.AddedOn = DateTime.Now;
+                StorePerson.UpdateOn = DateTime.Now;
+                StorePerson.AddedBy = login;
+                StorePerson.EmailId = getPerson.EmailId;
+                StorePerson.HomeCity = getPerson.HomeCity;
+
+                _db.People.Add(StorePerson);
+                _db.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
@@ -79,6 +113,37 @@ namespace SELab5.Controllers
                 // TODO: Add delete logic here
 
                 return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Contact(int id)
+        {
+            return View();
+        }
+
+        // POST: Person/Delete/5
+        [HttpPost]
+        public ActionResult Contact(int id, Contact personContact)
+        {
+          
+            try
+            {
+                // TODO: Add delete logic here
+                if (ModelState.IsValid)
+                {
+                    Contact addContact = new Contact();
+                    addContact.ContactNumber = personContact.ContactNumber;
+                    addContact.Type = personContact.Type;
+                    addContact.PersonId = id;
+                    _db.Contacts.Add(addContact);
+                    _db.SaveChanges();
+                }
+                    return RedirectToAction("Index");
+                
             }
             catch
             {
